3f88fabf5830a6968566bab4116023d4384d3600
justin-chen <justin.uob@gmail.com>
2017-03-07 17:35:45 +0800

    處理一個location

--- src/MyDomParser.java
+++ src/MyDomParser.java
@@ -27,94 +27,59 @@ public class MyDomParser {
 			NodeList locationList = doc.getElementsByTagName("location");
 
 			
-			// 拿第一個location
-			Node node = locationList.item(0);
-			Element location = (Element) node; 
+			// 處理一個location 
+			Node locationNode = locationList.item(0);
+			// System.out.println(n.getNodeName()); location
 			
-
+			Element locationElement = (Element) locationNode;
+			NodeList timeList = locationElement.getElementsByTagName("time");
+			System.out.println(timeList.getLength()); // 54
 			
+			String locationName = locationElement.getElementsByTagName("locationName").item(0).getTextContent(); // 淡海
+			String stationId = locationElement.getElementsByTagName("stationId").item(0).getTextContent(); // 11006
 			
-			NodeList details = location.getChildNodes();
-//			for(int j = 0; j<details.getLength(); j++){ // locationName, stationId, time, time, time ...
-//				Node n = details.item(j);
-//				if(n.getNodeType() == Node.ELEMENT_NODE){
-//					Element subTags = (Element) n;
-//					System.out.println(subTags.getTagName()); 
-//					
-//				}
-//			}
-			
-
-			//System.out.println(details.getLength()); // 113 // locationName, stationId, time, time, time ...
-			
-			/*
-			 *  #text
-				locationName
-				#text
-				stationId
-				#text
-				time
-				#text
-				time
-				#text
-				time
-			for(int i = 0; i<10; i++){
-				Node thirdNode = details.item(i);
-				System.out.println(thirdNode.getNodeName());
+			int counter = 0;
+			for(int i = 0; i<timeList.getLength(); i++){ // loop 54 time node
+				counter++;
+				Element timeElement = (Element) timeList.item(i);
+				String obsTime = timeElement.getElementsByTagName("obsTime").item(0).getTextContent();
+				String value = timeElement.getElementsByTagName("value").item(0).getTextContent();
+				System.out.print(counter+"  ");
+				System.out.println(locationName+"  "+stationId+"  "+obsTime+"  "+value); // 判斷 測站代號+資料日期，若已有資料則不寫入，無資料則寫入
+				
 			}
-			*/
-
-			Node timeNode = details.item(5);
-			// System.out.println(timeNode.getNodeName()); time
-			// <time> Element
-			Element timeElement = (Element) timeNode;
-			
-			Node obsTime = timeElement.getElementsByTagName("obsTime").item(0);
-			System.out.println(obsTime.getTextContent()); // 2017-03-06T04:54:00+08:00
-			
-			Element weatherElement = (Element)timeElement.getElementsByTagName("weatherElement").item(0);
-			Node valueNode = weatherElement.getElementsByTagName("value").item(0);
-			System.out.println(valueNode.getTextContent()); // 303
-			
-//			Element timeElement = (Element) timeNode;
-//			Node first = timeElement.getFirstChild();
-//			System.out.println(first.getNodeName());
-//			Node last = timeElement.getLastChild();
-//			System.out.println(last.getNodeName());
 			
 			
 			
 			
-			//System.out.println(time.getNodeName());
-			
-
-			
-			
-			
-			
-
 //			for(int i = 0; i<locationList.getLength(); i++){
 //				
-//				// 取  <locationName>淡海</locationName>
-//				// 	 <stationId>11006</stationId>
-//	
-//				Node node = locationList.item(i);
-//				if(node.getNodeType()==Node.ELEMENT_NODE){ // element node, text node...
-//					Element location = (Element) node; 
-//					NodeList details = location.getChildNodes();
-//					for(int j = 0; j<details.getLength(); j++){ // locationName, stationId, time, time, time ...
-//						Node n = details.item(j);
-//						if(n.getNodeType() == Node.ELEMENT_NODE){
-//							Element subTags = (Element) n;
-//							System.out.println(subTags.getTagName()); // locationName, stationId, time, time, time ...
-//							
-//						}
-//					}
-//					
-//				}
+//				Node n = locationList.item(0);
+//				System.out.println(n.getNodeName());
 //				
-//				System.out.println("=============================");
+//				
+////				Node node = locationList.item(i);
+////				if(node.getNodeType()==Node.ELEMENT_NODE){ // element node, text node...
+////					Element location = (Element) node; 
+////					NodeList details = location.getChildNodes();
+////					for(int j = 0; j<details.getLength(); j++){ // locationName, stationId, time, time, time ...
+////						Node n = details.item(j);
+////						if(n.getNodeType() == Node.ELEMENT_NODE){
+////							Element subTags = (Element) n;
+////							System.out.println(subTags.getTagName()); // locationName, stationId, time, time, time ...
+////						}
+////					}
+////				}
+//				
+//				System.out.println("=============== 一個測站，一組資料 ==============");
 //			}
+			
+			
+			
+			
+			
+			
+			
 			
 
 		} catch (ParserConfigurationException e) {
